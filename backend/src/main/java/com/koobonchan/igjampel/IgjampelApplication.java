2fc8057979e83227ccbf86e5e5a6c7d27bede8de
KooBonchan <garam9koo@gmail.com>
2025-02-28 14:18:54 +0900

    backend template without gradle setting

---
+++ backend/src/main/java/com/koobonchan/igjampel/IgjampelApplication.java
@@ -0,0 +1,13 @@
+package com.koobonchan.igjampel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IgjampelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IgjampelApplication.class, args);
+	}
+
+}
