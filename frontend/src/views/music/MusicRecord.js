a957ddd7afdaa70061a679f683f85fc00e2368d6
KooBonchan <garam9koo@gmail.com>
2025-03-26 17:08:14 +0900

    waveform

--- frontend/src/views/music/MusicRecord.js
+++ frontend/src/views/music/MusicRecord.js
@@ -3,17 +3,38 @@ import { Box, Button, ButtonGroup, Container, useMediaQuery, useTheme } from '@m
 import { blueGrey, red } from '@mui/material/colors';
 import { IconMetronome } from '@tabler/icons-react';
 import * as React from 'react';
-import sampleMp3 from '../../assets/musics/C Boogie Woogie.mp3'
+import sampleMp3 from '../../assets/musics/C Boogie Woogie.mp3';
+import WaveSurfer from 'wavesurfer.js';
 
 
 export default function MusicRecord() {
   const audioRef = React.useRef(new Audio(sampleMp3));
+  const waveformRef = React.useRef(null);
+  const wavesurferRef = React.useRef(null);
   const [isPlaying, setIsPlaying] = React.useState(false);
+
+  React.useEffect(() => {
+    wavesurferRef.current = WaveSurfer.create({
+      container: waveformRef.current,
+      waveColor: 'violet',
+      progressColor: 'purple',
+      cursorColor: 'navy',
+      barWidth: 2,
+      barRadius: 3,
+      height: 200,
+      width: 1000,
+      responsive: true,
+    });
+
+    wavesurferRef.current.load(sampleMp3);
+    return () => {wavesurferRef.current.destroy();}
+  }, []);
 
   const handlePlay = () => {
     audioRef.current.play();
     console.log('play');
     setIsPlaying(true);
+    wavesurferRef.current.setTime(audioRef.current.currentTime);
   }
   const handlePause = () => {
     audioRef.current.pause();
@@ -24,13 +45,25 @@ export default function MusicRecord() {
     audioRef.current.pause();
     audioRef.current.currentTime = 0;
     setIsPlaying(false);
+    wavesurferRef.current.setTime(0);
   }
 
+  React.useEffect(() => {
+    const syncWaveform = () => {
+      if (isPlaying) {
+        wavesurferRef.current.setTime(audioRef.current.currentTime);
+      }
+    };
+    const interval = setInterval(syncWaveform, 100); // Update every 100ms
+    return () => clearInterval(interval);
+  }, [isPlaying]);
 
   return (
     <>
       <MusicControls isPlaying={isPlaying} onPlay={handlePlay} onPause={handlePause} onStop={handleStop}/>
-      <Box width='90%' height='20vh' sx={{ backgroundColor: '#ccc', margin: '3rem auto' }} />
+      <Box width='90%' height='20vh' sx={{ backgroundColor: '#ccc', margin: '3rem auto' }}>
+        <div ref={waveformRef} style={{ backgroundColor: '#ccc' }} />
+      </Box>
       <Box width='90%' height='20vh' sx={{ backgroundColor: '#ccc', margin: '3rem auto' }} />
     </>
   );
@@ -82,4 +115,5 @@ const MusicControls = ({isPlaying, onPlay, onPause, onStop}) => {
     </Container>
   );
 };
+
 
